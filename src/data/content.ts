7330c0c624167b3dc7e89a5f5233cc5c81a77f52
Enguerran Weiss <enguerranweiss@gmail.com>
2022-11-22 08:58:49 +0100

    add mwe / pwn links

--- src/data/content.ts
+++ src/data/content.ts
@@ -1,7 +1,7 @@
 import { SiteContent } from "../types/site-content"
 
 export const siteInitialContent = ():SiteContent => ({
-  content: `Je m'appelle <strong>Enguerran</strong>, je suis <strong>développeur web front-end</strong>, et je travaille actuellement pour <a href="#">beta.gouv.fr</a> au sein de l'équipe <a href="https://immersion-facile.beta.gouv.fr/" target="_blank">Immersion Facilitée</a>. <small>#React #TypeScript #Redux #CSS #HTML #a11y</small>`,
+  content: `Je m'appelle <strong>Enguerran</strong>, je suis <strong>développeur web front-end</strong>, et je travaille actuellement pour <a href="#">beta.gouv.fr</a> au sein de l'équipe <a href="https://immersion-facile.beta.gouv.fr/" target="_blank">Immersion Facilitée</a>. Je suis également intervenant en <a href="https://sha.univ-poitiers.fr/masterweb/" target="_blank">Master Web éditorial et stratégie UX</a> à l'Université de Poitiers et je m'occupe de l'<a href="https://pwn-association.org/" target="_blank">association PWN</a>.<small>#React #TypeScript #Redux #CSS #HTML #a11y</small>`,
   links: [
     {
       type: 'github',
